e09479c556d51deab997929a453b3af03fab678d
Will Waldorf <02william.waldorf@gulllakecs.org>
2019-02-14 09:33:53 -0500

    Add Functions and Variables

--- Functions-Git.py
+++ Functions-Git.py
@@ -1,7 +1,7 @@
 #4.13.3: Greeting
 # Will Waldorf
 #2.6.19
-
+"""
 name = input("What is your name: ")
 
 def greeting():
@@ -9,3 +9,19 @@ def greeting():
     print ("It's nice to meet you ! ")
 
 greeting()
+"""
+
+
+# 4.13.4: Functions and Variables
+# Will Waldorf
+# 2.14.19
+
+x = 406
+
+def print_something():
+    x = 13
+    print(x)
+
+
+print_something()
+print(x)
