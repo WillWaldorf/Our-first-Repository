1b768c40e3fe45ce25c7da271a11bb0b317ee881
Will Waldorf <02william.waldorf@gulllakecs.org>
2019-02-14 09:16:09 -0500

    Add a Greetings Function

---
+++ Functions-Git.py
@@ -0,0 +1,11 @@
+#4.13.3: Greeting
+# Will Waldorf
+#2.6.19
+
+name = input("What is your name: ")
+
+def greeting():
+    print("Hi there " + name + "!")
+    print ("It's nice to meet you ! ")
+
+greeting()
