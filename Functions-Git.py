207def0ceb7d63dc70e12fea61b736f8e6d723d4
Will Waldorf <02william.waldorf@gulllakecs.org>
2019-02-27 10:04:26 -0500

    Add Functions & Variables, Part 3

--- Functions-Git.py
+++ Functions-Git.py
@@ -10,18 +10,43 @@ def greeting():
 
 greeting()
 """
-
+'''
 
 # 4.13.4: Functions and Variables
 # Will Waldorf
 # 2.14.19
 
-x = 11
+x = 11                  q
 
 def print_something():
     x = 13
     print(x)
-
+'''
 
 print_something()
 print(x)
+
+
+# 4.13.5: Functions & Variable - Part 2
+# Will Waldorf
+# 2.15.19
+my_variable = 3.6745
+
+def something():
+    print(my_variable + 10)
+
+something()
+
+
+# 4.13.6: Functions and Variables, Part 3
+# Will Waldorf
+# 2.18.19
+
+def print_number(x):
+    print(str(x))
+
+
+print_number(90)
+print_number('\n'+"Hello World ")
+
+
