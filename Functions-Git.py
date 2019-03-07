4d0e8fdbd582d58f8da494783e8d312156947db9
Will Waldorf <02william.waldorf@gulllakecs.org>
2019-03-07 14:56:05 -0500

    Take away merges

--- Functions-Git.py
+++ Functions-Git.py
@@ -16,7 +16,7 @@ greeting()
 # Will Waldorf
 # 2.14.19
 
-x = 11                  q
+x = 11                  
 
 def print_something():
     x = 13
@@ -110,11 +110,10 @@ def print_mutiple_times(string, times):
 
 print_mutiple_times('Hey good looking', 7)
 
-<<<<<<< HEAD
+
 #something different
 #more different
-=======
->>>>>>> Enter-Name-and-Age
+
 
 # 4.16.3: Enter a number
 # Will Waldorf
@@ -128,15 +127,13 @@ try:
 except ValueError:
     print('That was not an integer')
 
-<<<<<<< HEAD
+
 # 4.14.4: Name and Age
 # Will Waldorf
 # 2.18.19# 4.14.7: Print mutiple times
 # Will Waldorf
 # 2.19.19
-=======
-<<<<<<< HEAD
->>>>>>> test-master
+
 
 def print_mutiple_times(string, times):
     for i in range(times):
@@ -156,7 +153,7 @@ name_and_age('Mr Beast', 25)
 # Will Waldorf
 # 2.21.19
 
-<<<<<<< HEAD
+
 def retrieve_postive_number():
     while True:
         try:
@@ -170,10 +167,10 @@ def retrieve_postive_number():
             print('That was not a number, silly.')
 
 retrieve_postive_number()
-=======
+
 except ValueError:
     print('You must enter a float')
-=======
+
 # 4.16.4: Enter Name and Age
 # William Waldorf
 # 2.20.19
@@ -189,5 +186,4 @@ except ValueError:
 
 print('\n''Name: ', name)
 print('Age: ', age)
->>>>>>> Enter-Name-and-Age
->>>>>>> test-master
+
